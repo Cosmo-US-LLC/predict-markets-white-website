cd28061b3c6f98c85988da997b090e59b48eb531
Fergus Armstrong <fergus.armstrong1@gmail.com>
2026-04-24 15:13:33 +0100

    Updated wallet connect id

--- src/presale-gg/constants/config.js
+++ src/presale-gg/constants/config.js
@@ -1,4 +1,4 @@
 export const LISTING_PRICE = 0.03;
-export const WALLET_CONNECT_PROJECT_ID = "beaf54a517a11a4f99a8f9f36442a470";
+export const WALLET_CONNECT_PROJECT_ID = "c37cf3ea2421751d8f58daad34c5da5a";
 export const CARD_PARTNER_ID = "TODO:";
 export const CARD_IS_SANDBOX = false;
